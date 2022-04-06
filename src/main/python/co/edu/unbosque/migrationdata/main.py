358ec795787cd14455d99b26b485401a07caecde
alejo <alejo.80.123@gmail.com>
2022-04-06 08:48:21 -0500

    data text

--- src/main/python/co/edu/unbosque/migrationdata/main.py
+++ src/main/python/co/edu/unbosque/migrationdata/main.py
@@ -2,24 +2,40 @@ import src.main.python.co.edu.unbosque.textclassification.data_text as data_text
 import requests
 import json
 
-list = data_text.run("C:/Users/alejo/IdeaProjects/bd-2 data warehouse/src/resource/doc/fb_emp.xlsx")['datos']
 
-for dato in list:
-    json = {
-        "id": "",
-        "producto": dato["product"],
-        "text": dato["text"],
-        "likes": dato["likes"],
-        "comments": dato["comments"],
-        "shares": dato["shares"],
-        "calificacion": dato["value"],
-        "estado": "A"
-    }
-    resp = requests.post(
-        'https://gf45e9f189895df-data1warehouse.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/data/v1/publicaciones',
-        data=json)
+def cargarDatos(url):
+    try:
+        list = data_text.run(url)['datos']
+        print("Cargando...")
+        for dato in list:
+            json = {
+                "id": "",
+                "producto": dato["product"],
+                "text": dato["text"],
+                "likes": dato["likes"],
+                "comments": dato["comments"],
+                "shares": dato["shares"],
+                "calificacion": dato["value"],
+                "estado": "A"
+            }
+            requests.post(
+                'https://gf45e9f189895df-data1warehouse.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/data/v1'
+                '/publicaciones',
+                data=json)
+        print("Datos cargados")
+    except Exception as e:
+        print(e)
 
-responses = requests.get(
-    "https://gf45e9f189895df-data1warehouse.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/data/v1/publicaciones")
-returnItems = json.loads(responses.text)["items"]
-print(returnItems)
+
+def mostrarDatos():
+    try:
+        responses = requests.get(
+            "https://gf45e9f189895df-data1warehouse.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/data/v1"
+            "/publicaciones")
+        returnItems = json.loads(responses.text)["items"]
+        print(returnItems)
+    except Exception as e:
+        print(e)
+
+
+cargarDatos("C:/Users/alejo/IdeaProjects/bd-2 data warehouse/src/resource/doc/fb_emp.xlsx")
